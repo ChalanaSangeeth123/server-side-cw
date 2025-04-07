3dd62fc0d0f21ae34ebccbcacbf0bcc9bb74f7a8
Chalana Sangeeth <chalana.20210659@iit.ac.lk>
2025-04-07 14:11:03 +0530

    Update APIAuthMiddleWare.js

--- Middleware/APIAuth/APIAuthMiddleWare.js
+++ Middleware/APIAuth/APIAuthMiddleWare.js
@@ -1,30 +1,22 @@
-const APIKey= require('../../Services/ApiKeyService')
+const APIKeyService = require('../../Services/ApiKeyService');
 
-const apikeyMiddleware = async(req, res, next) =>{
-    //Get the key from the headers
-    const key = req.header('X-API-Key')
-    
-    if(!key)
-    {
-        return res.status(401).json({
-            error:'API Key Missing'
-        })
+const apikeyMiddleware = async (req, res, next) => {
+    const key = req.header('X-API-Key');
+    if (!key) {
+        return res.status(401).json({ error: 'API Key Missing' });
     }
-    this.apikeyservice = new APIKey()
-        try{
-            const data = await this.apikeyservice.validatekey(key)
-            if(!data.success)
-            {
-                return res.status(403).json({
-                    error:'Invalid key'
-                })
-            }
-            req.key = data.data
-            next()
-        }catch(error){
-            res.status(500).json({
-                error:error
-            })
+
+    const apikeyService = new APIKeyService();
+    try {
+        const data = await apikeyService.validatekey(key);
+        if (!data.success) {
+            return res.status(403).json({ error: 'Invalid or inactive API key' });
         }
-}
-module.exports = apikeyMiddleware
+        req.key = data.data;
+        next();
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+module.exports = apikeyMiddleware;
