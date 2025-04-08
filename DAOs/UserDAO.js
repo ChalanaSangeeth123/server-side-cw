9724b3b44c20883fee25da40951661f24bfd404d
Chalana Sangeeth <chalana.20210659@iit.ac.lk>
2025-04-08 07:35:19 +0530

    Update UserDAO.js

--- DAOs/UserDAO.js
+++ DAOs/UserDAO.js
@@ -1,82 +1,39 @@
-const pool = require('../Database/SQLCon')
-const {createResponse} = require('../Utilities/createResponse')
+const pool = require('../Database/SQLCon');
+const { createResponse } = require('../Utilities/createResponse');
+const { generateHash } = require('../Utilities/bcryptUtil');
 
-class UserDAO{
+class UserDAO {
+    constructor() {}
 
-    
-    constructor(){
-
-    }
-    createResponse(success, data = null, error = null) {
-        return {
-            success,
-            data,
-            error: error?.message || error
-        };
-    }
-    async create(req){
-       return new Promise((resolve, reject) =>{
-         pool.run('insert into users (email, password, fn, sn) values (?,?,?,?)', [...Object.values(req.body)], (err, rows) =>{
-            if(err)
-            {
-                reject(err)
-            }
-            resolve(createResponse(true, 'Record Inserted'))
-         })
-       })
-    }
-    async getByEmail(req){
-       try{
-            return new Promise((resolve, reject) =>{
-                pool.get('select * from users where email = (?)', [req.body.email], (err, rows)=>{
-                    if(err){
-                        reject(err)
-                    }
-                    resolve(createResponse(true, rows))
-                })
-            })
-       }catch(ex)
-       {
-            console.error(ex)
-       }
+    // Create new user with hashed password
+    async create(req) {
+        const { email, password, fn, sn } = req.body;
+        const hashedPassword = await generateHash(password);
+        return new Promise((resolve, reject) => {
+            pool.run(
+                'INSERT INTO users (email, password, fn, sn) VALUES (?, ?, ?, ?)',
+                [email, hashedPassword, fn, sn],
+                (err) => {
+                    if (err) reject(err);
+                    resolve(createResponse(true, 'User created'));
+                }
+            );
+        });
     }
 
-    async getAllUsers() {
-        try {
-            return new Promise((resolve, reject) => {
-                pool.all('SELECT id, email, fn, sn FROM users', [], (err, rows) => {
-                    if(err) {
-                        reject(err)
-                    }
-                    resolve(createResponse(true, rows))
-                })
-            })
-        } catch(ex) {
-            console.error(ex)
-            return this.createResponse(false, null, ex)
-        }
-    }
-
-    async getUserById(id) {
-        try {
-            return new Promise((resolve, reject) => {
-                pool.get('SELECT id, email, fn, sn FROM users WHERE id = ?', [id], (err, row) => {
-                    if(err) {
-                        reject(err)
-                    }
-                    if(!row) {
-                        resolve(this.createResponse(false, null, 'User not found'))
-                    } else {
-                        resolve(this.createResponse(true, row))
-                    }
-                })
-            })
-        } catch(ex) {
-            console.error(ex)
-            return this.createResponse(false, null, ex)
-        }
+    // Get user by email
+    async getByEmail(email) {
+        return new Promise((resolve, reject) => {
+            pool.get(
+                'SELECT * FROM users WHERE email = ?',
+                [email],
+                (err, row) => {
+                    if (err) reject(err);
+                    resolve(createResponse(true, row));
+                }
+            );
+        });
     }
 }
-
 
 module.exports = UserDAO;
