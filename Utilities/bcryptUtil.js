b10c99b00caa60a0e28413dc7ffcb61c0acd58f8
Chalana Sangeeth <chalana.20210659@iit.ac.lk>
2025-04-08 07:38:44 +0530

    Update bcryptUtil.js

--- Utilities/bcryptUtil.js
+++ Utilities/bcryptUtil.js
@@ -1,21 +1,23 @@
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
 
-const generateHash = async (string) =>{
-    const saltRounds = 10 //$2aod!234[hash][salt]
-    const salt = await bcrypt.genSalt(saltRounds)
-    const hashedpassword = await bcrypt.hash(string, salt)
-    return hashedpassword
-}
+const saltRounds = 10;
 
-const verify = async (formpassword, dbpassword) =>{
-    try{
-        const isMatch = await bcrypt.compare(formpassword, dbpassword)
-        return isMatch
+// Generate password hash
+const generateHash = async (string) => {
+    const salt = await bcrypt.genSalt(saltRounds);
+    const hashedpassword = await bcrypt.hash(string, salt);
+    return hashedpassword;
+};
+
+// Verify password
+const verify = async (formpassword, dbpassword) => {
+    try {
+        const isMatch = await bcrypt.compare(formpassword, dbpassword);
+        return isMatch;
+    } catch (ex) {
+        console.error(ex);
+        return false;
     }
-    catch(ex){
-        console.error(ex)
-    }
-}
+};
 
-
-module.exports = {generateHash, verify}
+module.exports = { generateHash, verify };
