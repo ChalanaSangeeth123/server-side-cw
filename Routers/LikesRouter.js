e72ae92dcdf5f678949bcc4bda8c57b796f97017
Chalana Sangeeth <chalana.20210659@iit.ac.lk>
2025-05-09 08:06:21 +0530

    Update LikesRouter.js

---
+++ Routers/LikesRouter.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const router = express.Router();
+const LikesService = require('../Services/LikesService');
+const checkSession = require('../Middleware/SessionAuth/SessionAuth');
+
+const likesService = new LikesService();
+
+router.post('/like', checkSession, async (req, res) => {
+    console.log('Like request received:', req.body); // Debug
+    const result = await likesService.like(req);
+    res.json(result);
+});
+
+router.get('/likes', async (req, res) => {
+    const result = await likesService.getLikes(req);
+    res.json(result);
+});
+
+module.exports = router;
