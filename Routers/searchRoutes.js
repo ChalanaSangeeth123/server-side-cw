16e432f85ead5a9eeffa0085b2a244a15a6b279c
Chalana Sangeeth <chalana.20210659@iit.ac.lk>
2025-05-09 08:07:07 +0530

    Update searchRoutes.js

--- Routers/searchRoutes.js
+++ Routers/searchRoutes.js
@@ -1,32 +1,32 @@
 const express = require('express');
 const router = express.Router();
-const BlogPostDAO = require('../DAO/BlogPostDAO');
+const BlogPostDAO = require('../DAOs/BlogPostDAO'); // Changed from './server-side/DAOs/BlogPostDAO'
 const CountryService = require('../Services/CountryService');
 
-router.get('/country/:country', async (req, res) => {
-  try {
-    const posts = await BlogPostDAO.getPostsByCountry(req.params.country);
-    const enrichedPosts = await Promise.all(posts.map(async (post) => {
-      const countryData = await CountryService.getCountryData(post.country);
-      return { ...post, countryData };
-    }));
-    res.status(200).json(enrichedPosts);
-  } catch (err) {
-    res.status(500).json({ error: 'Failed to search posts' });
-  }
+router.get('/country/:country', async (req, res, next) => {
+    try {
+        const posts = await BlogPostDAO.getPostsByCountry(req.params.country);
+        const enrichedPosts = await Promise.all(posts.map(async (post) => {
+            const countryData = await CountryService.getCountryData(post.country);
+            return { ...post, countryData };
+        }));
+        res.status(200).json(enrichedPosts);
+    } catch (err) {
+        next(err);
+    }
 });
 
-router.get('/user/:userId', async (req, res) => {
-  try {
-    const posts = await BlogPostDAO.getPostsByUser(req.params.userId);
-    const enrichedPosts = await Promise.all(posts.map(async (post) => {
-      const countryData = await CountryService.getCountryData(post.country);
-      return { ...post, countryData };
-    }));
-    res.status(200).json(enrichedPosts);
-  } catch (err) {
-    res.status(500).json({ error: 'Failed to search posts' });
-  }
+router.get('/user/:userId', async (req, res, next) => {
+    try {
+        const posts = await BlogPostDAO.getPostsByUser(req.params.userId);
+        const enrichedPosts = await Promise.all(posts.map(async (post) => {
+            const countryData = await CountryService.getCountryData(post.country);
+            return { ...post, countryData };
+        }));
+        res.status(200).json(enrichedPosts);
+    } catch (err) {
+        next(err);
+    }
 });
 
 module.exports = router;
